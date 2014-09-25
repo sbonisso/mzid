19c29b1e67108d9735bd879f172c8ace9f81f35e
sbonisso <sbonisso@ucsd.edu>
2014-09-25 14:56:21 -0700

    added alias to a default parser, set to batch parser, and added unecessary test case for this default

--- lib/mzid.rb
+++ lib/mzid.rb
@@ -1,12 +1,12 @@
 
 require 'mzid/psm'
-#require 'mzid/parser'
-#require 'mzid/parser_efficient'
+
 require 'mzid/base_parser'
 require 'mzid/batch_parser'
 require 'mzid/streaming_parser'
 
 module MzID
 
+  Parser = BatchParser
 
 end
